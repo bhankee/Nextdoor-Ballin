e5cd3e3a6ed17803d86c8264f83d0dcceeafc3ee
Brad Hankee <brad.hankee@gmail.com>
2018-07-31 17:12:23 -0400

    Updated .gitignore .

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+// KEYS
+module.exports = {
+  google: {
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET
+  },
+  session: {
+    cookieKey: process.env.SESSION_COOKIE
+  }
+};
